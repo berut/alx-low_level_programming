b1075168b9f3739ae6f5d38e60af33936a794ad1
berut <berutawitge19@gmail.com>
2022-03-10 12:53:04 -0800

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,8 @@
- #include <stdio.h>/** * main - prints text using puts() * * Return: Always 0 (Success) */int main(void){ puts("\"Programming is like building a multilingual puzzle"); return (0);}
+ #include <stdio.h>
+/** * main - prints text using puts()
+ *  
+ * Return: Always 0 (Success) */
+int main(void)
+{ puts("\"Programming is like building a multilingual puzzle");
+       	return (0);
+}
